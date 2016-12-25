87a331bd5edacda73d06989b24052f320f707447
Adam Grandquist <grandquista@gmail.com>
2016-12-25 08:02:53 -0800

    Fix errors squashing callbacks.

--- src/rethinkdb/cursor.lua
+++ src/rethinkdb/cursor.lua
@@ -99,7 +99,7 @@ local function each(state, var)
       if type(err) == 'table' then
         return 0, err
       end
-      return 0, errors.ReQLDriverError({}, err)  -- @todo
+      return 0, errors.ReQLDriverError({}, err)
     end
   end
   local row = state.it()
@@ -161,9 +161,10 @@ local function cursor(r, state, options, reql_inst)
   end
 
   --- Set or clear a callback for asynchronous processing of query results.
-  -- Calling without arguments clears any previous function set, and retains
-  -- results for future retrieval. This will only be called if another cursor
-  -- from the same connection is retrieving rows synchronously.
+  -- This callback will only be called if another
+  -- cursor from the same connection is retrieving rows synchronously. Calling
+  -- without arguments clears any previous function set, and retains results for
+  -- future retrieval.
   function cursor_inst.set(callback)
     state.outstanding_callback = callback
     if callback then
@@ -191,10 +192,11 @@ local function cursor(r, state, options, reql_inst)
   end
 
   --- Supports iteration with a for loop. First variable is an auto incrementing
-  -- integer starting at 1. This does not correspond with any data on the
-  -- server. If this is 0 then the row is an error. This error will be returned
-  -- forever and the caller is responsible for breaking out of the loop. The
-  -- second variable is a row result from the query or an error.
+  -- integer starting at 1. This integer does not correspond with any data on
+  -- the server. If this is 0 then the row is an error. This error will be
+  -- returned forever and the caller is responsible for breaking out of the
+  -- loop. The second variable is a row result from the query or an error.
+  -- Errors returned in a loop do not have valid links to the driver instance.
   function cursor_inst.each()
     cursor_inst.set()
     return each, state, 0
